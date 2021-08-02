07f55c53ca8138dc847ea2872d2150890c998b02
Freeman Jiang <freeman.jiang.ca@gmail.com>
2021-08-02 14:52:07 -0400

    Display API Data in card

--- public/js/findMatch.js
+++ public/js/findMatch.js
@@ -1,5 +1,8 @@
 import {API_KEY, SECRET} from './secrets.js'
 
+const content = document.querySelector('#content')
+console.log(content);
+
 const getAuthToken = async () => {
   const res = await axios.request({
     method: "post",
@@ -24,7 +27,32 @@ async function getData() {
 
   try {
     const {data} = await axios.get('https://api.petfinder.com/v2/animals?type=dog', config)
-    console.log(data)
+    const animal = data.animals[0]
+    console.log(animal);
+    content.innerHTML = `
+      <div class="columns is-centered">
+        <div class="column is-one-third">
+          <div class="card">
+              <div class="card-image">
+                  <figure class="image">
+                  ${animal.primary_photo_cropped ? `<img src='${animal.primary_photo_cropped.large}'/>` : false} 
+                  </figure>
+              </div>
+              <div class="card-content">
+                  <div class="media">
+                  <div class="media-content">
+                      <p class="title is-4">${animal.name}</p>
+                      <p class="is-6">${animal.gender}</p>
+                      <p class="is-6">${animal.breeds.primary}</p>
+                      <p class="subtitle is-6">${animal.age}</p>
+                  </div>
+                  </div>
+                  <div class="content">
+                  ${animal.description ? animal.description : 'No description available'}
+                  </div>
+              </div>
+          </div>
+      </div>`
   } catch (err) {
     console.log(err);
   }
