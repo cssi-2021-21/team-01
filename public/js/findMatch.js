5e3d243b73cb606f1bfb86f532cb3ae4fed2dc17
Freeman Jiang <freeman.jiang.ca@gmail.com>
2021-08-02 14:17:55 -0400

    OAuth working, getting data from API

--- public/js/findMatch.js
+++ public/js/findMatch.js
@@ -1,13 +1,20 @@
 import {API_KEY, SECRET} from './secrets.js'
 
-console.log(API_KEY, SECRET)
-
-const getAuthTokenAPI = () => {
-  // figure out how to return the OAuth Token
+const getAuthToken = async () => {
+  const res = await axios.request({
+    method: "post",
+    url: "https://api.petfinder.com/v2/oauth2/token",
+    data: {
+      "grant_type": "client_credentials",
+      "client_id": API_KEY,
+      "client_secret": SECRET
+    }
+  })
+  return res.data.access_token
 }
 
 async function getData() {
-  const OAUTH_TOKEN = getAuthTokenAPI()
+  const OAUTH_TOKEN = await getAuthToken()
   const config = {
     headers: {
       'Content-Type': 'application/json',
@@ -16,8 +23,8 @@ async function getData() {
   };
 
   try {
-    const res = await axios.get('https://api.petfinder.com/v2/animals?type=dog', config)
-    console.log(res)
+    const {data} = await axios.get('https://api.petfinder.com/v2/animals?type=dog', config)
+    console.log(data)
   } catch (err) {
     console.log(err);
   }
