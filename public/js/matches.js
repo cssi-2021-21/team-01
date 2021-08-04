3bf36d905824289345917c55ccc6c9097a72dc80
ethan-kwon <ethan200304@gmail.com>
2021-08-04 17:34:00 +0000

    match page pulls from database

--- public/js/matches.js
+++ public/js/matches.js
@@ -6,7 +6,7 @@ window.onload = (event) => {
     if (user) {
       console.log('Logged in as: ' + user.displayName);
       googleUserId = user.uid;
-    //   getCards(googleUserId);
+      getCards(googleUserId);
     } else {
       // If not logged in, navigate back to login page.
       window.location = 'index.html';
@@ -14,136 +14,156 @@ window.onload = (event) => {
   });
 };
 
-// const getCards = (userId) => {
-//     const matchRef = firebase.database().ref(`users/${userId}`);
-//     matchRef.on("value", (snapshot) => {
-//         const data = snapshot.val();
-//         renderDataAsHTML(data);
-//     })
-// }
-
-// const renderDataAsHTML = (data) => {
-//     let cards = ``;
-//     for (const matchItem in data) {
-//         const match = data[matchItem];
-//         // For each match create a card
-//         cards += createCard(match);
-//     };
-//     document.querySelector("#cards").innerHTML = cards;
-// }
-
-// const createCard = (match) => {
-//     return ``
-// }
-
-const cards = document.querySelector("#cards");
-
-const makeTag = (tag) => {
-  return `<span class="tag is-success is-light">${tag}</span>`
+const getCards = (userId) => {
+    const matchRef = firebase.database().ref(`users/${userId}/matches`);
+    matchRef.on("value", (snapshot) => {
+        const data = snapshot.val();
+        renderDataAsHTML(data);
+    })
 }
 
-const dummyData = [
-    {
-        name: "Barry",
-        breed: "Rotweiler",
-        age: "14",
-        gender: "Male",
-        size: "Medium",
-        description: "A cool dog",
-        tags: ["Friendly", "Affectionate", "Cheerful"]
-    },
-    {
-        name: "Tim",
-        breed: "Golden Retreiver",
-        age: "10",
-        gender: "Male",
-        size: "Large",
-        description: "A fancy dog",
-        tags: ["Cool", "Smart", "Cheerful"]
-    },
-    {
-        name: "Margaret",
-        breed: "Pitbull",
-        age: "5",
-        gender: "Female",
-        size: "Small",
-        description: "A dog",
-        tags: ["Cool"]
-    },
-    {
-        name: "Tim",
-        breed: "Golden Retreiver",
-        age: "10",
-        gender: "Male",
-        size: "Large",
-        description: "A fancy dog",
-        tags: ["Cool", "Smart", "Cheerful"]
-    },{
-        name: "Tim",
-        breed: "Golden Retreiver",
-        age: "10",
-        gender: "Male",
-        size: "Large",
-        description: "A fancy dog",
-        tags: ["Cool", "Smart", "Cheerful"]
-    },{
-        name: "Tim",
-        breed: "Golden Retreiver",
-        age: "10",
-        gender: "Male",
-        size: "Large",
-        description: "A fancy dog",
-        tags: ["Cool", "Smart", "Cheerful"]
-    },{
-        name: "Tim",
-        breed: "Golden Retreiver",
-        age: "10",
-        gender: "Male",
-        size: "Large",
-        description: "A fancy dog",
-        tags: ["Cool", "Smart", "Cheerful"]
-    },{
-        name: "Tim",
-        breed: "Golden Retreiver",
-        age: "10",
-        gender: "Male",
-        size: "Large",
-        description: "A fancy dog",
-        tags: ["Cool", "Smart", "Cheerful"]
-    },
-    {
-        name: "Tim",
-        breed: "Golden Retreiver",
-        age: "10",
-        gender: "Male",
-        size: "Large",
-        description: "A fancy dog",
-        tags: ["Cool", "Smart", "Cheerful"]
-    }
-]
+const renderDataAsHTML = (data) => {
+    let cards = ``;
+    for (const matchItem in data) {
+        const match = data[matchItem];
+        // For each match create a card
+        cards += createCard(match);
+    };
+    document.querySelector("#cards").innerHTML = cards;
+}
 
-dummyData.forEach(animal => {
-    cards.innerHTML += `<div class="column is-one-quarter">
+const createCard = (animal) => {
+    return `<div class="column is-one-quarter">
           <div class="card">
             <div class="card-image">
               <figure class="image">
-                <img src='${'/assets/dog-placeholder.png'}'/>
+                <img src='${animal.primary_photo_cropped ? animal.primary_photo_cropped.large : '../assets/dog-placeholder.png'}'/>
               </figure>
             </div>
             <div class="card-content">
               <div class="media">
                 <div class="media-content">
-                  <p class="title is-4">${animal.name}</p>
-                  ${animal.breed}</p>
-                  <p>${animal.age}  ${animal.gender}</p>
+                  <p class="title is-4">${animal.name || '' }</p>
+                  ${animal.breeds.primary || ''}</p>
+                  <p>${animal.age || ''}  ${animal.gender || ''}</p>
                   <p>Size as Adult: ${animal.size || ''}</p>
                 </div>
               </div>
                 <div class="content">
-                  ${animal.description}
+                  ${animal.description ? animal.description : 'No description available'}
                 </div>
                   ${animal.tags ? animal.tags.map(tag => makeTag(tag)) : ''}
             </div>
           </div>
         </div>`
-});
+}
+
+const makeTag = (tag) => {
+  return `<span class="tag is-success is-light">${tag}</span>`
+}
+
+// const dummyData = [
+//     {
+//         name: "Barry",
+//         breed: "Rotweiler",
+//         age: "14",
+//         gender: "Male",
+//         size: "Medium",
+//         description: "A cool dog",
+//         tags: ["Friendly", "Affectionate", "Cheerful"]
+//     },
+//     {
+//         name: "Tim",
+//         breed: "Golden Retreiver",
+//         age: "10",
+//         gender: "Male",
+//         size: "Large",
+//         description: "A fancy dog",
+//         tags: ["Cool", "Smart", "Cheerful"]
+//     },
+//     {
+//         name: "Margaret",
+//         breed: "Pitbull",
+//         age: "5",
+//         gender: "Female",
+//         size: "Small",
+//         description: "A dog",
+//         tags: ["Cool"]
+//     },
+//     {
+//         name: "Tim",
+//         breed: "Golden Retreiver",
+//         age: "10",
+//         gender: "Male",
+//         size: "Large",
+//         description: "A fancy dog",
+//         tags: ["Cool", "Smart", "Cheerful"]
+//     },{
+//         name: "Tim",
+//         breed: "Golden Retreiver",
+//         age: "10",
+//         gender: "Male",
+//         size: "Large",
+//         description: "A fancy dog",
+//         tags: ["Cool", "Smart", "Cheerful"]
+//     },{
+//         name: "Tim",
+//         breed: "Golden Retreiver",
+//         age: "10",
+//         gender: "Male",
+//         size: "Large",
+//         description: "A fancy dog",
+//         tags: ["Cool", "Smart", "Cheerful"]
+//     },{
+//         name: "Tim",
+//         breed: "Golden Retreiver",
+//         age: "10",
+//         gender: "Male",
+//         size: "Large",
+//         description: "A fancy dog",
+//         tags: ["Cool", "Smart", "Cheerful"]
+//     },{
+//         name: "Tim",
+//         breed: "Golden Retreiver",
+//         age: "10",
+//         gender: "Male",
+//         size: "Large",
+//         description: "A fancy dog",
+//         tags: ["Cool", "Smart", "Cheerful"]
+//     },
+//     {
+//         name: "Tim",
+//         breed: "Golden Retreiver",
+//         age: "10",
+//         gender: "Male",
+//         size: "Large",
+//         description: "A fancy dog",
+//         tags: ["Cool", "Smart", "Cheerful"]
+//     }
+// ]
+
+// dummyData.forEach(animal => {
+//     cards.innerHTML += `<div class="column is-one-quarter">
+//           <div class="card">
+//             <div class="card-image">
+//               <figure class="image">
+//                 <img src='${'/assets/dog-placeholder.png'}'/>
+//               </figure>
+//             </div>
+//             <div class="card-content">
+//               <div class="media">
+//                 <div class="media-content">
+//                   <p class="title is-4">${animal.name}</p>
+//                   ${animal.breed}</p>
+//                   <p>${animal.age}  ${animal.gender}</p>
+//                   <p>Size as Adult: ${animal.size || ''}</p>
+//                 </div>
+//               </div>
+//                 <div class="content">
+//                   ${animal.description}
+//                 </div>
+//                   ${animal.tags ? animal.tags.map(tag => makeTag(tag)) : ''}
+//             </div>
+//           </div>
+//         </div>`
+// });
